e1104d1079a3d3520e78aa68776a17345a5c2bc6
Rojibora <Rojibora@gmail.com>
2015-06-23 17:11:45 -0700

    Init Start
    * Oculus Rift start
    * Xbox controls for movement started

---
+++ Bleeding Edge/Assets/CameraPositionScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraPositionScript : MonoBehaviour {
+
+	[SerializeField] private Transform m_CharacterPosition;
+	[SerializeField] private float m_rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = m_CharacterPosition.position;
+		float horizontal = CrossPlatformInputManager.GetAxis("Look_Horizontal");
+
+		transform.Rotate( new Vector3( 0, horizontal * m_rotationSpeed, 0 ) );
+	}
+
+	void GetRotationFromInput() {
+
+	}
+}
