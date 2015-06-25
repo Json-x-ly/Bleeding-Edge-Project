0caff9c4399f0b940f86b6fcb867fad8935dc1ec
Rojibora <Rojibora@gmail.com>
2015-06-24 19:47:16 -0700

    Added Level 1-3 for Kris, Imported Petra flying drone

---
+++ Bleeding Edge/Assets/Scripts/OculusControlScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class OculusControlScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButtonDown ("Reset_Orient")) {
+			UnityEngine.VR.InputTracking.Recenter();
+			UnityEngine.VR.VRSettings.showDeviceView = true;
+		}
+	}
+}
