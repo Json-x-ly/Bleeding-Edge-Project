3558b1ac03e01b78e5d2e4586510ae6890054e87
Brandon Farrell <chees502@gmail.com>
2015-06-25 22:50:32 -0700

    Scanner no longer "Locked" to the path and will return to the point it was last at

--- Bleeding Edge/Assets/Scripts/ScannerLogic.cs
+++ Bleeding Edge/Assets/Scripts/ScannerLogic.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class ScannerLogic : MonoBehaviour {
     public BezierSpline path;
     public float t;
+    public enum _state { OnRail,HardTracking,SoftTracking}
+    public _state state = _state.OnRail;
+    public Vector3 target;
+    public float turnSpeed = 50;
+    public float moveSpeed = 2;
 	// Use this for initialization
     void Awake()
     {
@@ -19,8 +24,39 @@ public class ScannerLogic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        t = (t + Time.deltaTime * 0.05f) % 1;
-        transform.position = path.GetPoint(t);
-        transform.LookAt(transform.position + path.GetDirection(t));
+        switch(state){
+            case _state.OnRail:
+                //stuck to the rail
+                Vector3 railPoint = path.GetPoint(t);
+                if(SeekTarget(railPoint))
+                    t = (t + Time.deltaTime * 0.1f) % 1;
+                Debug.DrawLine(transform.position,railPoint);
+        break;
+            case _state.HardTracking:
+                //Activly move to the player
+        break;
+            case _state.SoftTracking:
+                //Randomly moving after player has not been seens
+        break;
+        }
 	}
+    bool SeekTarget(Vector3 target)
+    {
+        Vector3 dir = target - transform.position;
+        float dist = dir.magnitude;
+        dir.Normalize();
+        float rightDotDir = Vector3.Dot(transform.right,dir);
+        float forwardDotDir = Vector3.Dot(transform.forward,dir);
+        float upDotDir = -Vector3.Dot(transform.up, dir);
+        float spin = Vector3.Dot(-transform.right, Vector3.up);
+        Vector3 rot = new Vector3(upDotDir, rightDotDir, spin);
+        transform.Rotate(rot*Time.deltaTime*turnSpeed);
+        transform.position+=transform.forward*Time.deltaTime*moveSpeed*Mathf.Max(0,forwardDotDir);
+      
+        if (dist < 1f)
+        {
+            return true;
+        }
+        return false;
+    }
 }
