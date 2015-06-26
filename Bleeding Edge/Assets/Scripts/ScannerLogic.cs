b3da27da8c81771458b1c152322a3af35f067523
Brandon Farrell <chees502@gmail.com>
2015-06-25 21:39:10 -0700

    added DemoPath and DemoScanner to level 1 for example purposes

---
+++ Bleeding Edge/Assets/Scripts/ScannerLogic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScannerLogic : MonoBehaviour {
+    public BezierSpline path;
+    public float t;
+	// Use this for initialization
+    void Awake()
+    {
+        if (path == null)
+        {
+            Debug.LogError("No path assigned to the Scanner " + name + " removing GameObject");
+            Destroy(gameObject);
+        }
+    }
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t = (t + Time.deltaTime * 0.05f) % 1;
+        transform.position = path.GetPoint(t);
+        transform.LookAt(transform.position + path.GetDirection(t));
+	}
+}
