c81d8dc15d2a2b4106ebbab3720926a022ad65f7
Rojibora <Rojibora@gmail.com>
2015-07-02 17:20:59 -0700

    New player object, fan model reimported and animated

---
+++ Bleeding Edge/Assets/LevelColorSwap.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelColorSwap : MonoBehaviour {
+	public Renderer render;
+	public bool flip=true; 
+
+	// Use this for initialization
+	void Start () {
+		render = gameObject.GetComponent<Renderer>();
+		if (render == null){
+			Debug.Log ("This object has no Mesh Renderer");
+			Destroy (gameObject);
+		}
+		//render.material.SetColor ("_Outline Color", Color.red);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		DynamicGI.SetEmissive (GetComponent<Renderer> (), Color.red);
+		//if (flip) {
+		//	ToCyan();
+		//} else {
+		//	ToRed();
+		//}
+	}	
+
+	public void ToRed(){
+		//render.material.SetColor ("_Emission", Color.red);
+		//render.material.SetColor ("_Outline Color", Color.red);
+	}
+		
+	public void ToCyan() {
+		//render.material.SetColor ("_Emission", Color.cyan);
+		//render.material.SetColor ("_Outline Color", Color.cyan);
+	}
+}
---
+++ Bleeding Edge/Assets/OculusStartScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class OculusStartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake() {
+		VRSettings.loadedDevice = VRDeviceType.Oculus;
+		if (VRSettings.loadedDevice != VRDeviceType.None) {
+			VRSettings.enabled = true;
+		}
+	}
+}
