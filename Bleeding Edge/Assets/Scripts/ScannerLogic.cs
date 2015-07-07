cdc7c1b12e442e060312fe6676167f8116aac230
Brandon Farrell <chees502@gmail.com>
2015-07-06 22:58:49 -0700

    Scanners now avoid walls!

--- Bleeding Edge/Assets/Scripts/ScannerLogic.cs
+++ Bleeding Edge/Assets/Scripts/ScannerLogic.cs
@@ -27,6 +27,7 @@ public class ScannerLogic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        Avoidance();
         switch(state){
             case _state.OnRail:
                 //stuck to the rail
@@ -59,7 +60,7 @@ public class ScannerLogic : MonoBehaviour {
         float spin = Vector3.Dot(-transform.right, Vector3.up);
         Vector3 rot = new Vector3(upDotDir, rightDotDir, spin);
         transform.Rotate(rot*Time.deltaTime*turnSpeed);
-        transform.position+=transform.forward*Time.deltaTime*moveSpeed*Mathf.Max(0,forwardDotDir);
+        transform.position+=transform.forward*Time.deltaTime*moveSpeed*Mathf.Max(0.1f,forwardDotDir);
       
         if (dist < 1f)
         {
@@ -70,5 +71,35 @@ public class ScannerLogic : MonoBehaviour {
 			state=_state.HardTracking;
 		}
         return false;
+    }
+    void Avoidance()
+    {
+        //ugly code ahead!
+        Ray right = new Ray(transform.position,     (transform.forward + transform.right));
+        Ray left = new Ray(transform.position,      (transform.forward + -transform.right));
+        Ray up = new Ray(transform.position,        (transform.forward + transform.up));
+        Ray down = new Ray(transform.position,      (transform.forward + -transform.up));
+
+        float hori = IsHit(left);
+        hori-=IsHit(right);
+        float vert=IsHit(up);
+        vert-=IsHit(down);
+        transform.Rotate(new Vector3(vert, hori, 0) * Time.deltaTime*20);
+    }
+    float IsHit(Ray ray)
+    {
+        float dist = 4;
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, dist))
+        {
+
+            Debug.DrawRay(ray.origin, ray.direction * dist, Color.red);
+            return dist - hit.distance;
+        }
+        else
+        {
+            Debug.DrawRay(ray.origin, ray.direction * dist, Color.green);
+            return 0;
+        }
     }
 }
