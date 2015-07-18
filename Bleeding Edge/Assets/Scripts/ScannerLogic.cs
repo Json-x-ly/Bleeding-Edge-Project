f73c44a9f515dc0cdaedc44a10d8a02bb00f497c
Justin Tyrrell <jet1138@stu.aii.edu>
2015-07-18 06:01:56 -0700

    Updated Scanner Logic and Spotlight Detection with L.O.S.

--- Bleeding Edge/Assets/Scripts/ScannerLogic.cs
+++ Bleeding Edge/Assets/Scripts/ScannerLogic.cs
@@ -10,6 +10,8 @@ public class ScannerLogic : MonoBehaviour {
     public Vector3 target;
     public float turnSpeed = 50;
     public float moveSpeed = 2;
+	public float lineOfSightCoolDown = 0.0f;
+	const float MAX_LOS_CoolDown = 10.0f;
 	public float distToPlayer{
 		get{return Vector3.Distance (transform.position, PlayerLogic.main.transform.position); }
 	}
@@ -21,6 +23,11 @@ public class ScannerLogic : MonoBehaviour {
             Debug.LogError("No path assigned to the Scanner " + name + " removing GameObject");
             Destroy(gameObject);
         }
+
+		if (detector == null) {
+			Debug.Log ("Added Spotlight Detection Script");
+			detector = this.gameObject.AddComponent<SpotlightDetectionScript>();
+		}
     }
     void Start () {
 	
@@ -36,47 +43,51 @@ public class ScannerLogic : MonoBehaviour {
                 if(SeekTarget(railPoint))
                     t = (t + Time.deltaTime * 0.1f) % 1;
                 Debug.DrawLine(transform.position,railPoint);
-        break;
+        		break;
             case _state.HardTracking:
                 //Activly move to the player
-			SeekTarget(PlayerLogic.main.transform.position);
-			if(distToPlayer>30){
-				state=_state.OnRail;
-				MaterialRepo.TurnBlue();
-			}
-			if (distToPlayer< 5){
-				PlayerLogic.main.Die();
-				Debug.Log ("I am now dead. fucker");
-			}
+				SeekTarget(PlayerLogic.main.transform.position);
+				
+				IsTargetVisible();
 
-        break;
+				/*if(distToPlayer>30){
+					state=_state.OnRail;
+					MaterialRepo.TurnBlue();
+				}*/
+				if (distToPlayer< 5){
+					PlayerLogic.main.Die();
+					Debug.Log ("I am now dead. fucker");
+				}
+
+        		break;
             case _state.SoftTracking:
                 //Randomly moving after player has not been seens
-        break;
+        		break;
         }
 	}
     bool SeekTarget(Vector3 target)
     {
-        Vector3 dir = target - transform.position;
-        float dist = dir.magnitude;
-        dir.Normalize();
-        float rightDotDir = Vector3.Dot(transform.right,dir);
-        float forwardDotDir = Vector3.Dot(transform.forward,dir);
-        float upDotDir = -Vector3.Dot(transform.up, dir);
-        float spin = Vector3.Dot(-transform.right, Vector3.up);
-        Vector3 rot = new Vector3(upDotDir, rightDotDir, spin);
-        transform.Rotate(rot*Time.deltaTime*turnSpeed);
-        transform.position+=transform.forward*Time.deltaTime*moveSpeed*Mathf.Max(0.1f,forwardDotDir);
+		Vector3 dir = target - transform.position;
+		float dist = dir.magnitude;
+		dir.Normalize ();
+		float rightDotDir = Vector3.Dot (transform.right, dir);
+		float forwardDotDir = Vector3.Dot (transform.forward, dir);
+		float upDotDir = -Vector3.Dot (transform.up, dir);
+		float spin = Vector3.Dot (-transform.right, Vector3.up);
+		Vector3 rot = new Vector3 (upDotDir, rightDotDir, spin);
+		transform.Rotate (rot * Time.deltaTime * turnSpeed);
+		transform.position += transform.forward * Time.deltaTime * moveSpeed * Mathf.Max (0.1f, forwardDotDir);
       
-        if (dist < 1f)
-        {
-            return true;
-        }
-		Debug.Log (detector.playerDetected);
-		if (detector.playerDetected) {
-			MaterialRepo.TurnRed();
+		if (dist < 1.0f) {
+			return true;
+		}
+
+		if (detector.playerDetected == true) {
+			Debug.Log ("Detected Player!! " + detector.playerDetected);
+			MaterialRepo.TurnRed ();
 			state = _state.HardTracking;
 		}
+
         return false;
     }
     void Avoidance()
@@ -108,4 +119,52 @@ public class ScannerLogic : MonoBehaviour {
             return 0;
         }
     }
+
+	private bool IsTargetVisible() {
+		// Can we see the target?
+		Vector3 vectToTarget = PlayerLogic.main.transform.position - this.transform.position;
+		Ray rayToTarget = new Ray (transform.position, vectToTarget.normalized);
+		float dist = Vector3.Distance (this.transform.position, PlayerLogic.main.transform.position);
+		RaycastHit hitInfo;
+
+		if (Physics.Raycast (rayToTarget, out hitInfo, dist) == true) {
+			if (hitInfo.transform.tag == "Player") {
+				lineOfSightCoolDown = MAX_LOS_CoolDown;
+				Debug.Log("Player visible and actively tracking");
+				return true;
+			}
+			else  {
+				return LOSCoolDown();
+			}
+		} else {
+			return LOSCoolDown();
+		}
+	}
+
+	private bool LOSCoolDown() {
+		if(DecreaseLOSCoolDown() == false) {
+			Debug.Log("Lost track of player. Returning to rail.");
+			state = _state.OnRail;
+			MaterialRepo.TurnBlue();
+			return false;
+		}
+		return true;
+	}
+
+	private bool DecreaseLOSCoolDown() {
+
+		Debug.Log ("Can't see the player... Actively searching");
+		lineOfSightCoolDown -= Time.deltaTime;
+		
+		// How long have we not been able to see the player?
+		if(lineOfSightCoolDown <= 0.0f) {
+			lineOfSightCoolDown = 0.0f;
+			// Yes -> Go back to rails
+			return false;
+		}
+		else {
+			// No -> Continue tracking
+			return true;
+		}
+	}
 }
--- Bleeding Edge/Assets/Scripts/SpotlightDetectionScript.cs
+++ Bleeding Edge/Assets/Scripts/SpotlightDetectionScript.cs
@@ -8,10 +8,16 @@ public class SpotlightDetectionScript : MonoBehaviour {
 	public float distToPlayer{
 		get{return Vector3.Distance (transform.position, PlayerLogic.main.transform.position); }
 	}
-	private Light spotLight;
+	//private Light spotLight;
+	public Light spotLight;
 
 	void Awake () {
 		spotLight = gameObject.GetComponent<Light> ();
+		if (spotLight == null) {
+			Debug.Log ("Light found in child and added to script");
+			//spotLight = this.gameObject.AddComponent<Light>();
+			spotLight = this.gameObject.GetComponentInChildren<Light>();
+		}
 	}
 
 	// Update is called once per frame
@@ -25,8 +31,8 @@ public class SpotlightDetectionScript : MonoBehaviour {
 		Vector3 vectToTarget = PlayerLogic.main.transform.position - transform.position;
 		Vector3 forwardVect = transform.forward;
 		float angleToPlayer = Vector3.Angle (forwardVect, vectToTarget);
-		Debug.Log ("distance to player:" + distToPlayer);
-		Debug.Log ("angle to player:" + angleToPlayer + " >= " + spotLight.spotAngle * 0.5f);
+		//Debug.Log ("distance to player:" + distToPlayer);
+		//Debug.Log ("angle to player:" + angleToPlayer + " >= " + spotLight.spotAngle * 0.5f);
 		if (angleToPlayer >= spotLight.spotAngle * 0.5f)
 			return false;
 		Ray rayToTarget = new Ray (transform.position, vectToTarget.normalized);
@@ -40,8 +46,11 @@ public class SpotlightDetectionScript : MonoBehaviour {
 		if (Physics.Raycast (ray, out hit, dist)) {
 			Debug.DrawRay(transform.position, ray.direction * Vector3.Distance(transform.position, hit.transform.position), Color.red);
 			Debug.Log(hit.transform.name);
-			if (hit.transform.CompareTag("Player"))
+			if (hit.transform.tag == "Player")//("Player"))
+			{
+				Debug.Log ("Player tag found");
 				return true;
+			}
 			return false;
 		}
 		return false;
