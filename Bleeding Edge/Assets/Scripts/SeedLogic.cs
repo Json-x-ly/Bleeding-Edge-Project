136c423cd68725d7dbce09dbde0c310e7be7a96d
Brandon Farrell <chees502@gmail.com>
2015-07-19 20:20:08 -0700

    seed will now track the player when near, and remove it's self

---
+++ Bleeding Edge/Assets/Scripts/SeedLogic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeedLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dist = Vector3.Distance(transform.position, PlayerLogic.main.transform.position);
+        if (dist < 3)
+        {
+            Vector3 dir = (PlayerLogic.main.transform.position - transform.position).normalized;
+            transform.position += dir * Time.deltaTime*5;
+        }
+        if (dist < 0.2f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
